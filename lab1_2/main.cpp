dbc259af5ac3cd7aa1ee89696612f3185a1126fe
iktovr <vikvladbir@mail.ru>
2022-02-27 13:33:39 +0300

    Add lab 1.2

---
+++ lab1_2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
+#include "tridiagonal_matrix.hpp"
+
+int main() {
+	std::cout.precision(3);
+	std::cout << std::fixed;
+
+	size_t n;
+	std::cin >> n;
+
+	TDMatrix<double> a(n);
+	Vector<double> b(n);
+	std::cin >> a >> b;
+	std::cout << "Решение системы:\n" << a.Solve(b) << '\n';
+}
---
+++ lab1_2/tridiagonal_matrix.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+#include <iomanip>
+
+template <class T>
+using Vector = std::vector<T>;
+
+template <class T>
+class TDMatrix {
+private:
+	std::vector<std::vector<T>> _data;
+	size_t _size;
+
+public:
+	TDMatrix(size_t n) : _data(n, std::vector<T>(3)), _size(n) { }
+
+	std::vector<T>& operator[](size_t index) {
+		return _data[index];
+	}
+
+	const std::vector<T>& operator[](size_t index) const {
+		return _data[index];
+	}
+
+	size_t Size() const {
+		return _size;
+	}
+
+	Vector<T> Solve(const Vector<T>& b) {
+		if (b.size() != _size) {
+			return Vector<T>();
+		}
+
+		Vector<T> p(_size-1), q(_size-1), x(_size);
+		p[0] = - _data[0][2] / _data[0][1];
+		q[0] = b[0] / _data[0][1];
+
+		for (size_t i = 1; i < _size-1; ++i) {
+			p[i] = - _data[i][2] / (_data[i][1] + _data[i][0] * p[i-1]);
+			q[i] = (b[i] - _data[i][0] * q[i-1]) / (_data[i][1] + _data[i][0] * p[i-1]);
+		}
+
+		x[_size-1] = (b[_size-1] - _data[_size-1][0] * q[_size-2]) / (_data[_size-1][1] + _data[_size-1][0] * p[_size-2]);
+		for (int i = _size - 2; i >= 0; --i) {
+			x[i] = p[i] * x[i+1] + q[i];
+		}
+
+		return x;
+	}
+
+	template <class U>
+	friend std::ostream& operator<<(std::ostream&, const TDMatrix<U>&);
+	template <class U>
+	friend std::istream& operator>>(std::istream&, TDMatrix<U>&);
+};
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const TDMatrix<T>& matrix) {
+	os << matrix[0][1] << ' ' << matrix[0][2] << '\n';
+	for (size_t i = 1; i < matrix.Size()-1; ++i) {
+		for (size_t j = 0; j < 3; ++j) {
+			os << std::setw(8) << matrix[i][j] << ' ';
+		}
+		os << '\n';
+	}
+	os << matrix[matrix.Size()-1][0] << ' ' << matrix[matrix.Size()-1][1] << '\n';
+	return os;
+}
+
+template <class T>
+std::istream& operator>>(std::istream& is, TDMatrix<T>& matrix) {
+	is >> matrix[0][1] >> matrix[0][2];
+	for (size_t i = 1; i < matrix.Size()-1; ++i) {
+		for (size_t j = 0; j < 3; ++j) {
+			is >> matrix[i][j];
+		}
+	}
+	is >> matrix[matrix.Size()-1][0] >> matrix[matrix.Size()-1][1];
+	return is;
+}
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const Vector<T>& vec) {
+	for (size_t i = 0; i < vec.size(); ++i) {
+		os << std::setw(8) << vec[i] << ' ';
+	}
+	return os;
+}
+
+template <class T>
+std::istream& operator>>(std::istream& is, Vector<T>& vec) {
+	for (size_t i = 0; i < vec.size(); ++i) {
+		is >> vec[i];
+	}
+	return is;
+}
