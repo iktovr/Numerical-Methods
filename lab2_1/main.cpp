516a40189216dbd7ee11de39fa74df5928bb42c3
iktovr <vikvladbir@mail.ru>
2022-03-26 13:31:07 +0300

    Add lab 2.1

---
+++ lab2_1/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+#include <functional>
+#include <cmath>
+#include <vector>
+#include <utility>
+
+#include "solvers.hpp"
+
+int main() {
+	std::cout.precision(8);
+	std::cout << std::fixed;
+
+	std::function<double(double)> f = [](double x){ return std::log(x + 2) - x * x; };
+	std::function<double(double)> df = [](double x){ return 1.0 / (x + 2) - 2 * x; };
+	std::vector<interval_t<double>> intervals = {{-1.9, 0}, {0.5, 2}};
+
+	double eps;
+	std::cin >> eps;
+
+	double x0;
+
+	std::cout << "Метод простой итерации:\n";
+	for (auto interval: intervals) {
+		x0 = (interval.first + interval.second) / 2.0;
+		auto [x, iter_count] = IterationMethod(x0, interval, f, df, eps);
+
+		std::cout << "Корень: " << x << "\nКоличество итераций: " << iter_count << '\n';
+	}
+
+	std::cout << '\n';
+	std::cout << "Метод Ньютона:\n";
+	for (auto [a, b]: intervals) {
+		x0 = (a + b) / 2.0;
+		auto [x, iter_count] = NewtonMethod(x0, f, df, eps);
+
+		std::cout << "Корень: " << x << "\nКоличество итераций: " << iter_count << '\n';
+	}
+}
---
+++ lab2_1/solvers.hpp
@@ -0,0 +1,52 @@
+#include <functional>
+#include <cmath>
+
+template <class T>
+using interval_t = std::pair<T, T>;
+
+template <class T>
+int sign(T x) {
+	if (x > 0) {
+		return 1;
+	} else if (x < 0) {
+		return -1;
+	} else {
+		return 0;
+	}
+}
+
+template <class T>
+std::pair<T, size_t> IterationMethod(T x, const interval_t<T>& interval, const std::function<T(T)>& f, const std::function<T(T)>& df, double eps) {
+	T lambda = sign(df(x)) / std::max(std::abs(df(interval.first)), std::abs(df(interval.second)));
+	double q = std::abs(1 - lambda * sign(df(x)) * std::min(std::abs(df(interval.first)), std::abs(df(interval.second))));
+	q = q / (1 - q);
+
+	double eps_k;
+	T next_x;
+	size_t iter_count = 0;
+	do {
+		next_x = x - lambda * f(x);
+		eps_k = q * std::abs(next_x - x);
+		
+		std::swap(next_x, x);
+		++iter_count;
+	} while (eps_k > eps);
+
+	return {x, iter_count};
+}
+
+template <class T>
+std::pair<T, size_t> NewtonMethod(T x, const std::function<T(T)>& f, const std::function<T(T)>& df, double eps) {
+	double eps_k;
+	T next_x;
+	size_t iter_count = 0;
+	do {
+		next_x = x - f(x) / df(x);
+		eps_k = std::abs(next_x - x);
+		
+		std::swap(next_x, x);
+		++iter_count;
+	} while (eps_k > eps);
+
+	return {x, iter_count};
+}
