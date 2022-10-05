51caef791f1d459c59c6c4d497dafeb2b48f64a9
iktovr <vikvladbir@mail.ru>
2022-10-05 21:30:26 +0300

    Add boundary conditions for parabolic pde

--- include/partial_differential/parabolic_pde.hpp
+++ include/partial_differential/parabolic_pde.hpp
@@ -7,71 +7,158 @@
 #include "../linear/tridiagonal_matrix.hpp"
 #include "../linear/vector.hpp"
 
+const double PI = 2 * std::acos(0.0);
+
 namespace ParabolicPDE {
 	template <class T>
 	using grid_t = std::vector<std::vector<T>>;
 
 	template <class T>
-	std::tuple<std::vector<T>, std::vector<T>, grid_t<T>>
-	ExplicitSolver(T a, T b, T c, const std::function<T(T, T)>& f, T start, T end, T t_end, const std::function<T(T)>& psi, 
-	               const std::function<T(T)>& phi_start, const std::function<T(T)>& phi_end, int h_count, double sigma) {
+	struct PDE {
+		T a, b, c;
+		std::function<T(T, T)> f;
+		std::function<T(T)> psi;
+		T start, end;
+		T alpha1, beta1;
+		std::function<T(T)> gamma1;
+		T alpha2, beta2;
+		std::function<T(T)> gamma2;
+		std::function<T(T, T)> solution;
 
-		int tau_count = t_end * a * h_count * h_count / (end * end * sigma);
-		grid_t<T> u(tau_count + 1, std::vector<T>(h_count + 1));
+		PDE() = default;
+
+		PDE(T a, T b, T c, std::function<T(T, T)> f, std::function<T(T)> psi, T start, T end, 
+		    T alpha1, T beta1, std::function<T(T)> gamma1, T alpha2, T beta2, std::function<T(T)> gamma2) : 
+		    a(a), b(b), c(c), f(f), psi(psi), start(start), end(end), alpha1(alpha1), beta1(beta1), gamma1(gamma1), 
+		    alpha2(alpha2), beta2(beta2), gamma2(gamma2) {}
+
+		PDE(T a, T b, T c, std::function<T(T, T)> f) : 
+		    a(a), b(b), c(c), f(f) {}
+
+		void SetEquation(T a_, T b_, T c_, std::function<T(T, T)> f_) {
+			a = a_;
+			b = b_;
+			c = c_;
+			f = f_;
+		}
+
+		void SetBoundaries(std::function<T(T)> psi_, T start_, T end_, T alpha1_, T beta1_, std::function<T(T)> gamma1_, 
+			            T alpha2_, T beta2_, std::function<T(T)> gamma2_) {
+			psi = psi_;
+			start = start_;
+			end = end_;
+			alpha1 = alpha1_;
+			beta1 = beta1_;
+			gamma1 = gamma1_;
+			alpha2 = alpha2_;
+			beta2 = beta2_;
+			gamma2 = gamma2_;
+		}
+
+		void SetSolution(std::function<T(T, T)> solution_) {
+			solution = solution_;
+		}
+	};
+
+	enum class ApproxType : int {
+		Linear,
+		Quadratic,
+		Taylor
+	};
+
+	template <class T>
+	int CourantCondition(int h_count, double sigma, T t_end, T end, T a) {
+		return t_end * a * h_count * h_count / (end * end * sigma);
+	}
+
+	template <class T>
+	std::tuple<std::vector<T>, std::vector<T>>
+	GenerateGrid(const PDE<T>& pde, T t_end, int h_count, double sigma) {
+		int tau_count = CourantCondition(h_count, sigma, t_end, pde.end, pde.a);
 		std::vector<T> x(h_count + 1), t(tau_count + 1);
-		T h = (end - start) / h_count;
+		T h = (pde.end - pde.start) / h_count;
 		T tau = t_end / tau_count;
 
 		for (int i = 0; i <= h_count; ++i) {
-			x[i] = start + h * i;
+			x[i] = pde.start + h * i;
 		}
 		for (int k = 0; k <= tau_count; ++k) {
 			t[k] = tau * k;
 		}
 
+		return {x, t};
+	}
+
+	template <class T>
+	std::tuple<std::vector<T>, std::vector<T>, grid_t<T>>
+	ExplicitSolver(const PDE<T>& pde, T t_end, int h_count, double sigma, ApproxType type) {
+		auto [x, t] = GenerateGrid(pde, t_end, h_count, sigma);
+		return {x, t, ExplicitSolver(pde, x, t, t_end, type)};
+	}
+
+	template <class T>
+	grid_t<T> ExplicitSolver(const PDE<T>& pde, const std::vector<T>& x, const std::vector<T>& t, T t_end, ApproxType type) {
+		int h_count = x.size() - 1, tau_count = t.size() - 1;
+		grid_t<T> u(tau_count + 1, std::vector<T>(h_count + 1));
+		T h = (pde.end - pde.start) / h_count;
+		T tau = t_end / tau_count;
+
 		for (int i = 0; i <= h_count; ++i) {
-			u[0][i] = psi(x[i]);
-		}
-		for (int k = 1; k <= tau_count; ++k) {
-			u[k][0] = phi_start(t[k]);
-			u[k][h_count] = phi_end(t[k]);
+			u[0][i] = pde.psi(x[i]);
 		}
 
 		for (int k = 0; k < tau_count; ++k) {
 			for (int i = 1; i < h_count; ++i) {
 				double ddu = (u[k][i-1] - 2 * u[k][i] + u[k][i+1]) / (h * h);
 				double du = (u[k][i+1] - u[k][i-1]) / (2 * h);
-				u[k+1][i] = (a * ddu + b * du + c * u[k][i] + f(x[i], t[k])) * tau + u[k][i];
+				u[k+1][i] = (pde.a * ddu + pde.b * du + pde.c * u[k][i] + pde.f(x[i], t[k])) * tau + u[k][i];
+			}
+
+			if (type == ApproxType::Linear) {
+				u[k+1][0] = (pde.gamma1(t[k+1]) - pde.alpha1 / h * u[k+1][1]) / (-pde.alpha1 / h + pde.beta1);
+				u[k+1][h_count] = (pde.gamma2(t[k+1]) + pde.alpha2 / h * u[k+1][h_count-1]) / (pde.alpha2 / h + pde.beta2);
+
+			} else if (type == ApproxType::Quadratic) {
+				u[k+1][0] = (pde.gamma1(t[k+1]) - pde.alpha1 / (2 * h) * (4 * u[k+1][1] - u[k+1][2])) / (-3 * pde.alpha1 / (2 * h) + pde.beta1);
+				u[k+1][h_count] = (pde.gamma2(t[k+1]) - pde.alpha2 / (2 * h) * (u[k+1][h_count-2] - 4 * u[k+1][h_count-1])) / (3 * pde.alpha2 / (2 * h) + pde.beta2);
+
+			} else if (type == ApproxType::Taylor) {
+				T div = h - h * h * pde.b / (2 * pde.a),
+				  mult1 = (pde.c * h * h / (2 * pde.a) - 1 - h * h / (2 * pde.a * tau)),
+				  mult2 = h * h / (2 * pde.a);
+
+				u[k+1][0] = (pde.gamma1(t[k+1]) - pde.alpha1 * mult2 / div * (u[k][0] / tau + pde.f(x[0], t[k+1])) - pde.alpha1 / div * u[k+1][1]) / 
+				            (pde.alpha1 * mult1 / div + pde.beta1);
+
+				div = -h - h * h * pde.b / (2 * pde.a);
+				u[k+1][h_count] = (pde.gamma2(t[k+1]) - pde.alpha2 * mult2 / div * (u[k][h_count] / tau + pde.f(x[h_count], t[k+1])) - pde.alpha2 / div * u[k+1][h_count-1]) / 
+				            (pde.alpha2 * mult1 / div + pde.beta2);
 			}
 		}
-		return {t, x, u};
+		return u;
 	}
 
 	template <class T>
 	std::tuple<std::vector<T>, std::vector<T>, grid_t<T>>
-	ImplicitSolver(T a, T b, T c, const std::function<T(T, T)>& f, T start, T end, T t_end, const std::function<T(T)>& psi, 
-	               const std::function<T(T)>& phi_start, const std::function<T(T)>& phi_end, int h_count, double sigma) {
+	ImplicitSolver(const PDE<T>& pde, T t_end, int h_count, double sigma, ApproxType type) {
+		auto [x, t] = GenerateGrid(pde, t_end, h_count, sigma);
+		return {x, t, ImplicitSolver(pde, x, t, t_end, type)};
+	}
 
-		int tau_count = t_end * a * h_count * h_count / (end * end * sigma);
+	template <class T>
+	grid_t<T> ImplicitSolver(const PDE<T>& pde, const std::vector<T>& x, const std::vector<T>& t, T t_end, ApproxType type) {
+		int h_count = x.size() - 1, tau_count = t.size() - 1;
 		grid_t<T> u(tau_count + 1, std::vector<T>(h_count + 1));
-		std::vector<T> x(h_count + 1), t(tau_count + 1);
-		T h = (end - start) / h_count;
+		T h = (pde.end - pde.start) / h_count;
 		T tau = t_end / tau_count;
 
 		for (int i = 0; i <= h_count; ++i) {
-			x[i] = start + h * i;
-		}
-		for (int k = 0; k <= tau_count; ++k) {
-			t[k] = tau * k;
+			u[0][i] = pde.psi(x[i]);
 		}
 
-		for (int i = 0; i <= h_count; ++i) {
-			u[0][i] = psi(x[i]);
-		}
-
-		T alpha = (a / h - b / 2) * tau / h,
-		  beta = -1 - 2 * a * tau / (h * h) + c * tau,
-		  gamma = (a / h + b / 2) * tau / h;
+		T alpha = (pde.a / h - pde.b / 2) * tau / h,
+		  beta = -1 - 2 * pde.a * tau / (h * h) + pde.c * tau,
+		  gamma = (pde.a / h + pde.b / 2) * tau / h;
 
 		TDMatrix<T> matrix(h_count+1);
 		for (int i = 1; i < h_count; ++i) {
@@ -79,47 +166,74 @@ namespace ParabolicPDE {
 			matrix.b[i] = beta;
 			matrix.c[i] = gamma;
 		}
-		matrix.b[0] = 1;
-		matrix.b[h_count] = 1;
 
 		Vector<T> v(h_count+1);
 		for (int k = 0; k < tau_count; ++k) {
 			for (int i = 1; i < h_count; ++i) {
-				v[i] = -u[k][i] - tau * f(x[i], t[k]);
+				v[i] = -u[k][i] - tau * pde.f(x[i], t[k]);
 			}
-			v[0] = phi_start(t[k+1]);
-			v[h_count] = phi_end(t[k+1]);
+			v[0] = pde.gamma1(t[k+1]);
+			v[h_count] = pde.gamma2(t[k+1]);
+
+			if (type == ApproxType::Linear) {
+				matrix.b[0] = -pde.alpha1 / h + pde.beta1;
+				matrix.c[0] = pde.alpha1 / h;
+
+				matrix.a[h_count] = -pde.alpha2 / h;
+				matrix.b[h_count] = pde.alpha2 / h + pde.beta2;
+
+			} else if (type == ApproxType::Quadratic) {
+				T coeff = -pde.alpha1 / (2 * h) / gamma;
+				matrix.b[0] = -3 * pde.alpha1 / (2 * h) + pde.beta1 - coeff * alpha;
+				matrix.c[0] = 2 * pde.alpha1 / h - coeff * beta;
+				v[0] -= coeff * v[1]; 
+				
+				coeff = pde.alpha2 / (2 * h) / alpha;
+				matrix.a[h_count] = -2 * pde.alpha2 / h - coeff * beta;
+				matrix.b[h_count] = 3 * pde.alpha2 / (2 * h) + pde.beta2 - coeff * gamma;
+				v[h_count] -= coeff * v[h_count-1];
+			
+			} else if (type == ApproxType::Taylor) {
+				T div = h - h * h * pde.b / (2 * pde.a),
+				  mult1 = (pde.c * h * h / (2 * pde.a) - 1 - h * h / (2 * pde.a * tau)),
+				  mult2 = h * h / (2 * pde.a);
+
+				matrix.b[0] = pde.alpha1 * mult1 / div + pde.beta1;
+				matrix.c[0] = pde.alpha1 / div;
+				v[0] -= pde.alpha1 * mult2 / div * (u[k][0] / tau + pde.f(x[0], t[k+1]));
+
+				div = -h - h * h * pde.b / (2 * pde.a);
+				matrix.a[h_count] = pde.alpha2 / div;
+				matrix.b[h_count] = pde.alpha2 * mult1 / div + pde.beta2;
+				v[h_count] -= pde.alpha2 * mult2 / div * (u[k][h_count] / tau + pde.f(x[h_count], t[k+1]));
+			}
 
 			u[k+1] = matrix.Solve(v);
 		}
-		return {t, x, u};
+		return u;
 	}
 
 	template <class T>
 	std::tuple<std::vector<T>, std::vector<T>, grid_t<T>>
-	CombinedSolver(T a, T b, T c, const std::function<T(T, T)>& f, T start, T end, T t_end, const std::function<T(T)>& psi, 
-	               const std::function<T(T)>& phi_start, const std::function<T(T)>& phi_end, int h_count, double sigma, double theta) {
+	CombinedSolver(const PDE<T>& pde, T t_end, int h_count, double sigma, ApproxType type, double theta) {
+		auto [x, t] = GenerateGrid(pde, t_end, h_count, sigma);
+		return {x, t, CombinedSolver(pde, x, t, t_end, type, theta)};
+	}
 
-		int tau_count = t_end * a * h_count * h_count / (end * end * sigma);
+	template <class T>
+	grid_t<T> CombinedSolver(const PDE<T>& pde, const std::vector<T>& x, const std::vector<T>& t, T t_end, ApproxType type, double theta) {
+		int h_count = x.size() - 1, tau_count = t.size() - 1;
 		grid_t<T> u(tau_count + 1, std::vector<T>(h_count + 1));
-		std::vector<T> x(h_count + 1), t(tau_count + 1);
-		T h = (end - start) / h_count;
+		T h = (pde.end - pde.start) / h_count;
 		T tau = t_end / tau_count;
 
 		for (int i = 0; i <= h_count; ++i) {
-			x[i] = start + h * i;
-		}
-		for (int k = 0; k <= tau_count; ++k) {
-			t[k] = tau * k;
+			u[0][i] = pde.psi(x[i]);
 		}
 
-		for (int i = 0; i <= h_count; ++i) {
-			u[0][i] = psi(x[i]);
-		}
-
-		T alpha = theta * (a / h - b / 2) * tau / h,
-		  beta = -1 + theta * (-2 * a * tau / (h * h) + c * tau),
-		  gamma = theta * (a / h + b / 2) * tau / h;
+		T alpha = theta * (pde.a / h - pde.b / 2) * tau / h,
+		  beta = -1 + theta * (-2 * pde.a * tau / (h * h) + pde.c * tau),
+		  gamma = theta * (pde.a / h + pde.b / 2) * tau / h;
 
 		TDMatrix<T> matrix(h_count+1);
 		for (int i = 1; i < h_count; ++i) {
@@ -127,23 +241,53 @@ namespace ParabolicPDE {
 			matrix.b[i] = beta;
 			matrix.c[i] = gamma;
 		}
-		matrix.b[0] = 1;
-		matrix.b[h_count] = 1;
 
 		Vector<T> v(h_count+1);
 		for (int k = 0; k < tau_count; ++k) {
 			for (int i = 1; i < h_count; ++i) {
 				double ddu = (u[k][i-1] - 2 * u[k][i] + u[k][i+1]) / (h * h);
 				double du = (u[k][i+1] - u[k][i-1]) / (2 * h);
-				double uik = a * ddu + b * du + c * u[k][i] + f(x[i], t[k]);
-				v[i] = -u[k][i] - tau * (theta * f(x[i], t[k]) + (1 - theta) * uik);
+				double uik = pde.a * ddu + pde.b * du + pde.c * u[k][i] + pde.f(x[i], t[k]);
+				v[i] = -u[k][i] - tau * (theta * pde.f(x[i], t[k]) + (1 - theta) * uik);
 			}
-			v[0] = phi_start(t[k+1]);
-			v[h_count] = phi_end(t[k+1]);
+			v[0] = pde.gamma1(t[k+1]);
+			v[h_count] = pde.gamma2(t[k+1]);
+
+			if (type == ApproxType::Linear) {
+				matrix.b[0] = -pde.alpha1 / h + pde.beta1;
+				matrix.c[0] = pde.alpha1 / h;
+
+				matrix.a[h_count] = -pde.alpha2 / h;
+				matrix.b[h_count] = pde.alpha2 / h + pde.beta2;
+
+			} else if (type == ApproxType::Quadratic) {
+				T coeff = -pde.alpha1 / (2 * h) / gamma;
+				matrix.b[0] = -3 * pde.alpha1 / (2 * h) + pde.beta1 - coeff * alpha;
+				matrix.c[0] = 2 * pde.alpha1 / h - coeff * beta;
+				v[0] -= coeff * v[1];
+				
+				coeff = pde.alpha2 / (2 * h) / alpha;
+				matrix.a[h_count] = -2 * pde.alpha2 / h - coeff * beta;
+				matrix.b[h_count] = 3 * pde.alpha2 / (2 * h) + pde.beta2 - coeff * gamma;
+				v[h_count] -= coeff * v[h_count-1];
+			
+			} else if (type == ApproxType::Taylor) {
+				T div = h - h * h * pde.b / (2 * pde.a),
+				  mult1 = (pde.c * h * h / (2 * pde.a) - 1 - h * h / (2 * pde.a * tau)),
+				  mult2 = h * h / (2 * pde.a);
+
+				matrix.b[0] = pde.alpha1 * mult1 / div + pde.beta1;
+				matrix.c[0] = pde.alpha1 / div;
+				v[0] -= pde.alpha1 * mult2 / div * (u[k][0] / tau + pde.f(x[0], t[k+1]));
+
+				div = -h - h * h * pde.b / (2 * pde.a);
+				matrix.a[h_count] = pde.alpha2 / div;
+				matrix.b[h_count] = pde.alpha2 * mult1 / div + pde.beta2;
+				v[h_count] -= pde.alpha2 * mult2 / div * (u[k][h_count] / tau + pde.f(x[h_count], t[k+1]));
+			}
 
 			u[k+1] = matrix.Solve(v);
 		}
-		return {t, x, u};
+		return u;
 	}
 }
-
--- lab5/lab5_cli.cpp
+++ lab5/lab5_cli.cpp
@@ -6,55 +6,55 @@
 
 using namespace ParabolicPDE;
 
-const double PI = 2 * std::acos(0.0);
-
 int main() {
-	double a = 1, b = 0, c = 0;
-	auto f = [](double, double){ return 0; };
-	double start = 0, end = 1, t_end;
-	auto psi = [](double x){ return std::sin(PI * x); };
-	auto phi_start = [](double){ return 0; };
-	auto phi_end = [](double){ return 0; };
-	auto u = [&a](double x, double t){ return std::exp(- a * PI * PI * t) * std::sin(PI * x); };
+	PDE<double> pde{
+		1., 1., -1., [](double, double){ return 0; }
+	};
+	pde.SetBoundaries(
+		[](double x){ return sin(x); },
+		0., PI,
+		1., 1., [&pde](double t){ return exp((pde.c - pde.a) * t) * (cos(pde.b * t) + sin(pde.b * t)); },
+		1., 1., [&pde](double t){ return -exp((pde.c - pde.a) * t) * (cos(pde.b * t) + sin(pde.b * t)); });
+	pde.SetSolution(
+		[&pde](double x, double t){ return exp((pde.c - pde.a) * t) * sin(x + pde.b * t); });
 
 	int n;
-	double sigma;
-	double theta = 0.5;
+	double t_end, sigma, theta = 0.5;
 	std::cin >> t_end >> n >> sigma;
 
 	std::vector<double> t, x;
-	grid_t<double> u_impl, u_expl, u_comb;
+	grid_t<double> u_expl, u_impl, u_comb;
 	std::cout << std::setprecision(12) << std::fixed;
-	std::tie(t, x, u_impl) = ImplicitSolver<double>(a, b, c, f, start, end, t_end, psi, phi_start, phi_end, n, sigma);
-	std::tie(t, x, u_expl) = ExplicitSolver<double>(a, b, c, f, start, end, t_end, psi, phi_start, phi_end, n, sigma);
-	std::tie(t, x, u_comb) = CombinedSolver<double>(a, b, c, f, start, end, t_end, psi, phi_start, phi_end, n, sigma, theta);
+	std::tie(x, t, u_expl) = ExplicitSolver<double>(pde, t_end, n, sigma, ApproxType::Taylor);
+	u_impl = ImplicitSolver<double>(pde, x, t, t_end, ApproxType::Taylor);
+	u_comb = CombinedSolver<double>(pde, x, t, t_end, ApproxType::Taylor, theta);
 
 	std::vector<double> error_impl(t.size()), error_expl(t.size()), error_comb(t.size());
     for (size_t k = 0; k < t.size(); ++k) {
         double max_error_impl = 0, max_error_expl = 0, max_error_comb = 0;
         for (size_t i = 0; i < x.size(); ++i) {
-            max_error_impl = std::max(max_error_impl, std::abs(u(x[i], t[k]) - u_impl[k][i]));
-            max_error_expl = std::max(max_error_expl, std::abs(u(x[i], t[k]) - u_expl[k][i]));
-            max_error_comb = std::max(max_error_comb, std::abs(u(x[i], t[k]) - u_comb[k][i]));
+            max_error_impl = std::max(max_error_impl, std::abs(pde.solution(x[i], t[k]) - u_impl[k][i]));
+            max_error_expl = std::max(max_error_expl, std::abs(pde.solution(x[i], t[k]) - u_expl[k][i]));
+            max_error_comb = std::max(max_error_comb, std::abs(pde.solution(x[i], t[k]) - u_comb[k][i]));
         }
-        error_impl[k] = max_error_impl;
         error_expl[k] = max_error_expl;
+        error_impl[k] = max_error_impl;
         error_comb[k] = max_error_comb;
     }
-
-	std::cout << "$implicit << EOD\n";
-	for (size_t k = 0; k < t.size(); ++k) {
-		for (size_t i = 0; i < x.size(); ++i) {
-			std::cout << t[k] << ' ' << x[i] << ' ' << u_impl[k][i] << '\n';
-		}
-		std::cout << '\n';
-	}
-	std::cout << "EOD\n";
 
 	std::cout << "$explicit << EOD\n";
 	for (size_t k = 0; k < t.size(); ++k) {
 		for (size_t i = 0; i < x.size(); ++i) {
 			std::cout << t[k] << ' ' << x[i] << ' ' << u_expl[k][i] << '\n';
+		}
+		std::cout << '\n';
+	}
+	std::cout << "EOD\n";
+
+	std::cout << "$implicit << EOD\n";
+	for (size_t k = 0; k < t.size(); ++k) {
+		for (size_t i = 0; i < x.size(); ++i) {
+			std::cout << t[k] << ' ' << x[i] << ' ' << u_impl[k][i] << '\n';
 		}
 		std::cout << '\n';
 	}
@@ -72,7 +72,7 @@ int main() {
 	std::cout << "$solution << EOD\n";
 	for (size_t k = 0; k < t.size(); ++k) {
 		for (size_t i = 0; i < x.size(); ++i) {
-			std::cout << t[k] << ' ' << x[i] << ' ' << u(x[i], t[k]) << '\n';
+			std::cout << t[k] << ' ' << x[i] << ' ' << pde.solution(x[i], t[k]) << '\n';
 		}
 		std::cout << '\n';
 	}
@@ -80,11 +80,11 @@ int main() {
 
 	std::cout << "$errors << EOD\n";
 	for (size_t k = 0; k < t.size(); ++k) {
-		std::cout << t[k] << ' ' << error_impl[k] << '\n';
+		std::cout << t[k] << ' ' << error_expl[k] << '\n';
 	}
 	std::cout << '\n';
 	for (size_t k = 0; k < t.size(); ++k) {
-		std::cout << t[k] << ' ' << error_expl[k] << '\n';
+		std::cout << t[k] << ' ' << error_impl[k] << '\n';
 	}
 	std::cout << '\n';
 	for (size_t k = 0; k < t.size(); ++k) {
@@ -98,12 +98,12 @@ int main() {
 	             "set pm3d depthorder border lc \"white\" lt 1 lw 0.5\nset palette rgbformulae 33,13,10\n";
 
 	std::cout << "set xlabel \"t\"\nset ylabel \"x\"\nset zlabel \"u(x, t)\"\n"
-	             "set title \"Implicit scheme\"\n"
-	             "splot $implicit with pm3d\n\n";
-
-	std::cout << "set xlabel \"t\"\nset ylabel \"x\"\nset zlabel \"u(x, t)\"\n"
 	             "set title \"Explicit scheme\"\n"
 	             "splot $explicit with pm3d\n\n";
+
+	std::cout << "set xlabel \"t\"\nset ylabel \"x\"\nset zlabel \"u(x, t)\"\n"
+	             "set title \"Implicit scheme\"\n"
+	             "splot $implicit with pm3d\n\n";
 
 	std::cout << "set xlabel \"t\"\nset ylabel \"x\"\nset zlabel \"u(x, t)\"\n"
 	             "set title \"Combined scheme\"\n"
